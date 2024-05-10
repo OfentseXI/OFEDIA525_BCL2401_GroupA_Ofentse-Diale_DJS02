8779c48821ae327a38687aee47d22b31e59d7c2a
OfentseXI <dofentse69@gmail.com>
2024-05-10 05:54:04 +0200

    code cleaning, debugging and addition of commentary

--- scripts.js
+++ scripts.js
@@ -5,36 +5,34 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
-  console.log(dividend);
-  console.log(divider)
-
-  let divideResult = dividend / divider;
-
-  if ( divideResult % 1 !== 0) {
-    divideResult = Math.floor(divideResult);
-  };
-
-  if ( dividend === "" || divider === "") {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
-    return;
-  };
-
-  if ( isNaN(dividend) || isNaN(divider) ) {
-    console.error("Invalid input values", new Error("Input values should be numbers.") );
-    result.classList.add("critical-error");
-    result.innerText = "Something critical went wrong. Please reload the page.";
-    return;
-  };
-
-  if ( divider === "0" ) {
-
-    console.error("Invalid divider", new Error("Divided by Zero"));
+  
+  result.innerText = Math.floor(dividend / divider);
+  result.classList.remove("error-message");
+  
+  // Invalid division should throw error in the console
+  if (divider === "0") {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
     result.classList.add("error-message");
-    result.innerText = "Division not performed. Invalid number provided. Try again.";
+    throw new Error (
+      "Invalid division, cannot use 0"
+    );
+  }
 
-    return;
-  };
+  // Validation when values are missing
+  else if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    result.classList.add("error-message");
+  } 
+  
+  // Providing any input that is not a number should crash the program
+  else if (isNaN(divider) || isNaN(dividend)) {
+    const div = document.createElement('div');
+    div.classList.add('critical-error');
+    div.textContent = `Something critical went wrong. Please reload the page`;
+    
+    document.body.append(div)
+    throw new Error ("Both inputs should be numbers");
+    
+  }
 
-  result.innerText = dividend / divider;
 });
